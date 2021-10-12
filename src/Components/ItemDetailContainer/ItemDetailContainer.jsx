141dc3bf8b3f02e568fef4c3fa629ff8adcb4633
maribelalarcon <maribelsoledadalarcon@gmail.com>
2021-10-12 20:39:33 -0300

    Ultimos detalles

--- src/Components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/Components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { getProducto } from '../../utils/promesas'
 import ItemDetail from '../ItemDetailContainer/ItemDetail';
 import { useParams } from 'react-router-dom';
+import { Spinner, Button } from 'react-bootstrap';
 
 export default function ItemDetailContainer() {
     const [producto, setProducto] = useState({})
@@ -18,6 +19,22 @@ export default function ItemDetailContainer() {
 
     return (
         <div className="p-5">
+            {/* <>
+                <Button variant="primary" disabled>
+                    <Spinner
+                    as="span"
+                    animation="border"
+                    size="sm"
+                    role="status"
+                    aria-hidden="true"
+                    />
+                    <span className="visually-hidden">
+                        {loading ? <h2> Cargando Productos</h2> : <ItemDetail producto={producto} /> }
+                    </span>
+                </Button>{' '}
+            
+            </> */}
+
             {loading ? <h2> Cargando Productos</h2> : <ItemDetail producto={producto} /> }
         </div>
     )
